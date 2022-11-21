a2dbc4dd7e5599068f540051933ed6cdce42869b
Margo <m.yunanova@gmail.com>
2022-11-22 00:20:45 +0300

    implement class popupWithForm; #20

---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,39 @@
+import Popup from './Popup';
+
+export default class PopupWithForm extends Popup {
+  constructor(selector, submitForm) {
+    super(selector);
+    this.submitForm = submitForm;
+    this.form = this.popup.querySelector('.form');
+    this.formInputs = this.form.querySelectorAll('.form__item');
+    this.buttonSubmit = this.popup.querySelector('.form__submit-button');
+  }
+
+  #getInputValues() {
+    const inputsValue = {};
+    for (const input of this.formInputs) {
+      inputsValue[input.id] = input.value;
+    }
+    return inputsValue;
+  }
+
+  close() {
+    this.form.reset();
+    super.close();
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this.form.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      this.buttonSubmit.textContent = 'Сохранение...';
+      this.submitForm(this.#getInputValues()).then(() => {
+        this.close();
+      }).catch((err) => {
+        console.log(err);
+      }).finally(() => {
+        this.buttonSubmit.textContent = 'Сохранение';
+      });
+    });
+  }
+}
