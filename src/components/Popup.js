e00e19da8804cea8d3a37a55b72b066b8abb1916
Margo <m.yunanova@gmail.com>
2022-11-22 00:19:37 +0300

    correct close method name; Создать класс Popup #18

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -4,30 +4,30 @@ export default class Popup {
     this.buttonClose = this.popup.querySelector('.popup__icon-close');
   }
 
-  openPopup() {
+  open() {
     this.popup.classList.add('popup_opened');
     document.addEventListener('keydown', this.#handleEscClose);
   }
 
-  closePopup() {
+  close() {
     this.popup.classList.remove('popup_opened');
     document.removeEventListener('keydown', this.#handleEscClose);
   }
 
   #handleEscClose = (evt) => {
     if (evt.key === 'Escape') {
-      this.closePopup();
+      this.close();
     }
   }
 
   setEventListeners() {
     this.buttonClose.addEventListener('click', () => {
-      this.closePopup();
+      this.close();
     });
 
     this.popup.addEventListener('click', (evt) => {
       if (evt.target.classList.contains('popup_opened')) {
-        this.closePopup();
+        this.close()
       }
     });
   }
