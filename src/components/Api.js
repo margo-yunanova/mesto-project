156e2dfd7a66c38d7c43e7165c047b36b6c155a4
Aria1ink <107484275+Aria1ink@users.noreply.github.com>
2022-11-26 20:30:09 +0300

    api after review

--- src/components/Api.js
+++ src/components/Api.js
@@ -12,68 +12,68 @@ export default class Api {
     };
   }
 
+  #request(url, options) {
+    return fetch(url, options).then(this.#checkPromiseResult);
+  }
+
   getProfile () {
-    return fetch(`${this.options.baseUrl}/users/me`, {
-      headers: this.options.headers
-    }).then(this.#checkPromiseResult);
+    return this.#request(`${this.options.baseUrl}/users/me`, {headers: this.options.headers});
   };
 
   getInitialCards () {
-    return fetch(`${this.options.baseUrl}/cards`, {
-      headers: this.options.headers
-    }).then(this.#checkPromiseResult);
+    return this.#request(`${this.options.baseUrl}/cards`, {headers: this.options.headers});
   };
 
   pushProfileUpdate (profileName, profileBio) {
-    return fetch(`${this.options.baseUrl}/users/me`, {
+    return this.#request(`${this.options.baseUrl}/users/me`, {
       method: 'PATCH',
       headers: this.options.headers,
       body: JSON.stringify({
         'name': profileName,
         'about': profileBio,
       })
-    }).then(this.#checkPromiseResult);
+    });
   };
 
   pushNewPlaceCard (placeName, placeLink) {
-    return fetch(`${this.options.baseUrl}/cards`, {
+    return this.#request(`${this.options.baseUrl}/cards`, {
       method: 'POST',
       headers: this.options.headers,
       body: JSON.stringify({
         'name': placeName,
         'link': placeLink,
       })
-    }).then(this.#checkPromiseResult);
+    });
   };
 
   deletePlaceCard (cardId) {
-    return fetch(`${this.options.baseUrl}/cards/${cardId}`, {
+    return this.#request(`${this.options.baseUrl}/cards/${cardId}`, {
       method: 'DELETE',
       headers: this.options.headers,
-    }).then(this.#checkPromiseResult);
+    });
   };
 
   likeCard (cardId) {
-    return fetch(`${this.options.baseUrl}/cards/likes/${cardId}`, {
+    return this.#request(`${this.options.baseUrl}/cards/likes/${cardId}`, {
       method: 'PUT',
       headers: this.options.headers,
-    }).then(this.#checkPromiseResult);
+    });
   };
 
   deleteLikeCard (cardId) {
-    return fetch(`${this.options.baseUrl}/cards/likes/${cardId}`, {
+    return this.#request(`${this.options.baseUrl}/cards/likes/${cardId}`, {
       method: 'DELETE',
       headers: this.options.headers,
-    }).then(this.#checkPromiseResult);
+    });
   };
 
   updateUserPic (userPicLink) {
-    return fetch(`${this.options.baseUrl}/users/me/avatar`, {
+    return this.#request(`${this.options.baseUrl}/users/me/avatar`, {
       method: 'PATCH',
       headers: this.options.headers,
       body: JSON.stringify({
         'avatar': userPicLink
       })
-    }).then(this.#checkPromiseResult);
+    });
   };
 };
