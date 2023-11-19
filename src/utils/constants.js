b7410f2a3e99de72f73fa8b283355280fd5951d3
Margo <m.yunanova@gmail.com>
2023-11-19 15:41:31 +0300

    fix baseUrl

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -17,7 +17,7 @@ export const profileEditUserPic = page.querySelector(".profile__edit-userpic");
 export const formValidators = {};
 
 export const api = new Api({
-  baseUrl: "http://mesto.margo.nomoredomainsmonster.ru/",
+  baseUrl: "http://mesto.margo.nomoredomainsmonster.ru",
   headers: {
     authorization: "3b45e7df-7420-41ed-89ad-84bab9964bf8",
     "Content-Type": "application/json",
